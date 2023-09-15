38d89d2f7086f6894c7a260a55ca743ad9168b78
aydin :^) <a219201085@unison.mx>
2023-09-15 11:34:09 -0700

    form and output

--- we-split/ContentView.swift
+++ we-split/ContentView.swift
@@ -8,16 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State var check = 0.0
+    @State var people = 0
+    @State var tip = 0
+    @FocusState var isCheckFocused: Bool
+    
+    let currency = Locale.current.currency?.identifier ?? "USD"
+    
+    var total: Double {
+        let tip = check / 100 * Double(tip)
+        return tip + check
     }
+    
+    var splittedTotal: Double {
+        let people = Double(people+2)
+        return total/people
+    }
+     
+    var body: some View {
+        return NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $check, format: .currency(code: currency))
+                    .keyboardType(.decimalPad)
+                    .focused($isCheckFocused)
+                    Picker("Number of people", selection: $people) {
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tip) {
+                        ForEach(0...100, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                header: {
+                    Text("how much tip do you want to leave?")
+                }
+                Section {
+                    Text(splittedTotal, format: .currency(code: currency))
+                }
+                header: {
+                    Text("Amount per person")
+                }
+                Section {
+                    Text(total, format: .currency(code: currency))
+                }
+                header: {
+                    Text("Total amount")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isCheckFocused = false
+                    }
+                }
+            }
+        }
+    }
+    
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ we-split/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  we-split
+//
+//  Created by aydin salman on 18/09/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        return NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
