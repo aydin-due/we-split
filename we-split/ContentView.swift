7a675ac0105e00a5071ebac2d2e87646d058fc2d
aydin :^) <a219201085@unison.mx>
2023-09-15 11:34:09 -0700

    form and output

--- we-split/ContentView.swift
+++ we-split/ContentView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var check = 0.0
+    @State var people = 0
+    @State var tip = 20
+    @FocusState var isCheckFocused: Bool
+    
+    let tips = [0, 10, 15, 20, 25]
+    
+    var splittedTotal: Double {
+        let people = Double(people+2)
+        let tip = check / 100 * Double(tip)
+        let total = tip + check
+        return total/people
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        return NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $check, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($isCheckFocused)
+                    Picker("Number of people", selection: $people) {
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                Section {
+                    Picker("Tip percentage", selection: $tip) {
+                        ForEach(tips, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                header: {
+                    Text("how much tip do you want to leave?")
+                }
+                Section {
+                    Text(splittedTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isCheckFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
